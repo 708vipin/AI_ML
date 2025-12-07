2fc2fc2691ec4d1d86c05d6340751338f4b724d5
pvipin708 <pvipin708@gmail.com>
2025-12-07 13:57:50 +0530

    fundamentals_

---
+++ python_fundamentals_1/Q8.py
@@ -0,0 +1,7 @@
+#Take the radius(r) as user input and print the area
+
+#Use the formula Area = pi *r^2, where pi = 3.14
+
+radius = float(input("Enter the radius of the circle: "))
+area = 3.14*(radius)**2
+print("The area of a circle of radius", radius, "is", area)
---
+++ python_fundamentals_1/Q9.py
@@ -0,0 +1,10 @@
+#Ask the user for: Principal(P), Rate(R), Time(T). Convert all to FLOAT and compute simple interest
+#Formula is, S.I = (P*R*T)/100
+
+principal_P = float(input("Enter the Principal Amount: "))
+rate_R = float(input("Enter the interest rate: "))
+time_T = float(input("Enter the time in year: "))
+
+simple_interest =(principal_P*rate_R*time_T)/100
+
+print("The simple interest is,", simple_interest)

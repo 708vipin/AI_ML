52775671599ae3a84824da7c9e628f21534252f4
pvipin708 <pvipin708@gmail.com>
2025-12-15 07:44:37 +0530

    dictionary

---
+++ python_fundamentals_3/Q4.py
@@ -0,0 +1,8 @@
+#Given a tuple of integers
+#Create a tuple of odd integers
+#Create a tuple of even integers
+
+
+a = (2,3,4,5,6,7,8,9,10,11, 14,13,16,17)
+
+
---
+++ python_fundamentals_3/Q5.py
@@ -0,0 +1,15 @@
+#Create a dictionary where
+#Keys = student names
+#values = marks (Integers)
+#Write a menu based program where user presses a key ("A","B","C","D") depending upon the operation they want to perform on the dictionary
+# A - Add a student
+# B - update marks
+# C - search for a student
+# D - Display all students and marks
+
+stu_dict = {
+    "Rahul": 90,
+    "Vipin": 100
+}
+
+print(type(stu_dict))

1e9fdf4bb1e8700e18feaf7c10be1031483ebcf0
pvipin708 <pvipin708@gmail.com>
2026-04-23 16:29:40 +0530

    layout

--- projects_module/intelligent_AI_attendance_system_face & voice/src/components/header.py
+++ projects_module/intelligent_AI_attendance_system_face & voice/src/components/header.py
@@ -2,4 +2,11 @@ import streamlit as st
 
 
 def header_home():
-    st.header('Snap Class')
+
+    logo_url = "C:\Users\pvipi\Desktop\VIP_GitHub\AI_ML\projects_module\intelligent_AI_attendance_system_face & voice\images\logo.png"
+    st.markdown("""
+              
+              
+              
+              
+              """, unsafe_allow_html = True)

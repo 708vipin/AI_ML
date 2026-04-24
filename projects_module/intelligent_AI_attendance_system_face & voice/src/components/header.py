3c93b207402592641dd83e9dd9387a7a361c2df4
pvipin708 <pvipin708@gmail.com>
2026-04-24 15:48:57 +0530

    edit

--- projects_module/intelligent_AI_attendance_system_face & voice/src/components/header.py
+++ projects_module/intelligent_AI_attendance_system_face & voice/src/components/header.py
@@ -3,10 +3,13 @@ import streamlit as st
 
 def header_home():
 
-    logo_url = "C:\Users\pvipi\Desktop\VIP_GitHub\AI_ML\projects_module\intelligent_AI_attendance_system_face & voice\images\logo.png"
-    st.markdown("""
-              
-              
+    logo_url = r"C:\Users\pvipi\Desktop\VIP_GitHub\AI_ML\projects_module\intelligent_AI_attendance_system_face & voice\images\logo.png"
+    
+    st.markdown(f"""
+
+                <div>
+              <img src='{logo_url}' height:100px; />
+              </div>
               
               
               """, unsafe_allow_html = True)

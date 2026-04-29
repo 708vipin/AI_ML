c53de81dc196a4e5502cf32ebfd5964473a0b26d
pvipin708 <pvipin708@gmail.com>
2026-04-29 10:07:17 +0530

    layout_done

--- projects_module/intelligent_AI_attendance_system_face & voice/src/components/footer.py
+++ projects_module/intelligent_AI_attendance_system_face & voice/src/components/footer.py
@@ -12,3 +12,17 @@ def footer_home():
                     
               
               """, unsafe_allow_html = True)
+    
+
+
+def footer_dashboard():
+
+   
+    
+    st.markdown(f"""
+           <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center;">
+                <p style="font-weight:bold; color:black;"> Created with ❤️ by Vipin </p>
+                </div>
+                    
+              
+              """, unsafe_allow_html = True)
--- projects_module/intelligent_AI_attendance_system_face & voice/src/components/header.py
+++ projects_module/intelligent_AI_attendance_system_face & voice/src/components/header.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def header_home():
 
-    logo_url = r"https://github.com/shradha-khapra/ai-attendance-project-landing/blob/main/static/img/logo.png?raw=true" 
+    logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
     
     st.markdown(f"""
 
@@ -11,6 +11,23 @@ def header_home():
 
               <img src='{logo_url}' style='height:100px;' />
               <h1 style ='text-align:center; color:#E0E3FF'>SNAP <br/> CLASS</h1> 
+              </div>
+              
+              
+              """, unsafe_allow_html = True)
+    
+
+    
+def header_dashboard():
+
+    logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
+    
+    st.markdown(f"""
+
+              <div style="display:flex; align-items:center; justify-content:center; gap:10px">
+
+              <img src='{logo_url}' style='height:85px;' />
+              <h2 style ='text-align:left; color:#5865F2'>SNAP <br/> CLASS</h2> 
               </div>
               
               
